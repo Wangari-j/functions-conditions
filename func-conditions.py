23518571f2a7e687024d16c574e50ab1880f86ce
Wangari-j <wangarijudy@gmail.com>
2024-02-27 23:31:53 +0300

    conditions

---
+++ func-conditions.py
@@ -0,0 +1,21 @@
+def prevent_distraction(str):
+
+    if str == anime:
+        return "No!"
+    
+    elif str == vines:
+        return "No!"
+    
+    elif str == roasts:
+        return "No!"
+    
+    elif str == Danny_Devito:
+        return "NO!"
+    
+    else:
+        return "Safe watching"
+    
+print(prevent_distraction(anime))
+
+
+    
